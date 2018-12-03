a49ba5d0c572dcf97bb8436d3cea37ad71774436
StephenKairos <stephenkairos.reyes@sjsu.edu>
2018-12-02 17:52:20 -0800

    "Committing Dummy File to Force Login Sequence, Date-Time: " Sun 12/02/2018 17:52:20.16

--- init-git.bat
+++ init-git.bat
@@ -2,16 +2,19 @@ echo "Initializing Identity for New Machine"
 git config --global user.email "stephenkairos.reyes@sjsu.edu"
 git config --global user.name "StephenKairos"
 echo "Dummy File to Force Login Sequence" > dummy.txt
-set datetime=%DATE% %TIME%
 git add .
-git commit -m "Committing Dummy File to Force Login Sequence, Date-Time: $datetime"
+echo "Committing Dummy File to Force Login Sequence, Date-Time: " %DATE% %TIME% > datetime.txt
+git commit -F datetime.txt
+del datetime.txt
 git pull
 git push
 del dummy.txt
 echo "Deleting Dummy file..."
 set datetime=%DATE% %TIME%
 git add .
-git commit -m "Committing Dummy File to Force Login Sequence, Date-Time: $datetime"
+echo "Deleting Dummy File to Force Login Sequence, Date-Time: " %DATE% %TIME% > datetime.txt
+git commit -F datetime.txt
+del datetime.txt
 git pull
 git push	
 cmd /k
